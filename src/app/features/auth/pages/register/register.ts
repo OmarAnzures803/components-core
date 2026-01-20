49b08e7e2cf52eb7bf5470f2a7915de157a40324
CrafterJe <rauljs05123456789@gmail.com>
2026-01-20 17:58:15 -0600

    Added Register Component & UI

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
@@ -7,11 +6,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
-import { AuthService } from '../../../services/auth';
-
+import { AuthService } from '../../services/auth';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   standalone: true,
   imports: [
     FormsModule,
@@ -19,12 +17,13 @@ import { AuthService } from '../../../services/auth';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
-],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+    MatInputModule,
+  ],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
 })
-export class LoginComponent {
+export class RegisterComponent {
+  name = '';
   email = '';
   password = '';
   loading = false;
@@ -39,17 +38,18 @@ export class LoginComponent {
     this.loading = true;
     this.error = null;
 
-    this.authService.login(this.email, this.password).subscribe({
+    // Todos los usuarios se registran como 'customer' por defecto
+    this.authService.register(this.name, this.email, this.password, 'customer').subscribe({
       next: () => {
         this.loading = false;
-        this.router.navigateByUrl('/products'); // o '/admin' si es admin
+        // Redirigir después de registro exitoso
+        this.router.navigateByUrl('/products');
       },
       error: (err) => {
-        console.error('Error de login', err);
-        this.error = 'Correo o contraseña incorrectos.';
+        console.error('Error de registro', err);
+        this.error = err.error?.message || 'Error al crear la cuenta. Intenta nuevamente.';
         this.loading = false;
       },
     });
   }
 }
-
